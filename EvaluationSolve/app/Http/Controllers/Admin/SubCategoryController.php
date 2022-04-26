a5f93db34526fbbf455467e0dea747bce10de0df
fahimtayebee9 <fahimtayebee9@gmail.com>
2022-04-26 21:45:44 +0600

    SubCategory Controller Update

--- EvaluationSolve/app/Http/Controllers/Admin/SubCategoryController.php
+++ EvaluationSolve/app/Http/Controllers/Admin/SubCategoryController.php
@@ -4,82 +4,105 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
+use App\Models\SubCategory;
+use App\Models\Category;
 
 class SubCategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        session([
+            'active_menu' => 'category',
+            'document_title' => 'Categories ',
+        ]);
+
+        $list_items = Category::all();
+        return view('admin.category.index', compact('list_items'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $validated = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category_id' => 'required|integer',
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validated->errors()->first(),
+            ]);
+        }
+        else{
+            $category               = new SubCategory();
+            $category->title        = $request->title;
+            $category->description  = $request->description;
+            $category->category_id  = $request->category_id;
+            $category->save();
+
+            return redirect()->route('category.index')->with([
+                'status' => 'success',
+                'message' => 'Category has been created.',
+            ]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $validated = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category_id' => 'required|integer',
+        ]);
+
+        $sub_categoryObj = Category::find($id);
+
+        if ($validated->fails()) {
+            return redirect()->back()
+                ->withErrors($validated)
+                ->withInput();
+        }
+        else if($sub_categoryObj){
+            $sub_categoryObj->name         = $request->name;
+            $sub_categoryObj->title        = $request->title;
+            $sub_categoryObj->description  = $request->description;
+            $sub_categoryObj->category_id  = $request->category_id;
+            $sub_categoryObj->update();
+
+            return redirect()->route('category.index')->with([
+                'status' => 'success',
+                'message' => 'Category has been updated.',
+            ]);
+        }
+        else{
+            return redirect()->route('category.index')->with([
+                'status' => 'error',
+                'message' => 'Category not found.',
+            ]);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $sub_sub_categoryObj = SubCategory::find($id);
+
+        if($sub_sub_categoryObj){
+            $sub_sub_categoryObj->delete();
+
+            return redirect()->route('category.index')->with([
+                'status' => 'success',
+                'message' => 'Category has been deleted.',
+            ]);
+        }
+        else{
+            return redirect()->route('category.index')->with([
+                'status' => 'error',
+                'message' => 'Category not found.',
+            ]);
+        }
     }
 }
